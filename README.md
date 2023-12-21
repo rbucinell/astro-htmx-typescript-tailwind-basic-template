fa8eb23614fb745f91c9f17f00498f5f44e9bfba
Ryan Bucinell <rbucinell.dev@gmail.com>
2023-12-21 12:23:48 -0500

    project init

---
+++ README.md
@@ -0,0 +1,12 @@
+#Astro-HTMX-Typescript-Tailwind Baisc Template
+A barebones scaffold for starting an Astro/HTMX/Typescript/Tailwind project. 
+
+### Installation
+```
+npm install
+```
+
+### Run Project
+```
+npm run dev
+```
---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import htmx from 'astro-htmx';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [ htmx(), tailwind() ],
+});
